1f9902bf0eb3a047c4a71bbf18bf6558c7f03a5d
Ольга <evinn86@mail.ru>
2022-02-17 16:52:11 +0700

    Добавьте файлы проекта.

---
+++ Modul1.Zadanie23/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            FactorialAsync(n);
+            Console.ReadKey();
+        }
+        //  void метод сразу выводит
+        static void Factorial(int n) 
+        {
+            int k = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                k *= i;
+                Thread.Sleep(20);  
+            }
+            Console.WriteLine("Факториал числа {0}! = {1}", n, k);
+        }
+        // метод асинхронный
+        static async void FactorialAsync(int n)
+        {
+            // метод вызывает Fact(n), Run- на выходе получаем задачу 
+            await Task.Run(() => Factorial(n));  
+        }
+    }
+}
